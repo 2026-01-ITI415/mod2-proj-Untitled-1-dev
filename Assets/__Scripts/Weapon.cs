28be5a2c6d4d95cb24123d67c6dbf268b269b228
Niamiko <eemikolaj@gmail.com>
2026-05-12 22:30:20 -0400

    I give up

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -140,6 +140,16 @@ public class Weapon : MonoBehaviour
                 p.vel = p.transform.rotation * vel;
                 break;
 
+            case eWeaponType.laser:
+                p = MakeProjectile();
+                p.vel = vel;
+                break;
+
+            case eWeaponType.missile:
+                p = MakeProjectile();
+                p.vel = vel;
+                break;
+
         }
     }
 
